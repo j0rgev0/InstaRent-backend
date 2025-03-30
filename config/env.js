a9bd770be71fb3729a82c13e5027e95ea523f61a
Jorge <jorgevivar2005@gmail.com>
2025-03-30 21:17:20 +0200

    Update configuration files

--- config/env.js
+++ config/env.js
@@ -5,5 +5,9 @@ config()
 export const env = {
   DATABASE_URL: process.env.DATABASE_URL,
   SIGNATURE: process.env.SIGNATURE,
-  PORT: process.env.PORT ?? 3000
+  PORT: process.env.PORT ?? 3000,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
 }
