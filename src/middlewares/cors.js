75c9f65c22d4b8e7bf9ee76ed827689eda5a8827
Jorge <jorgevivar2005@gmail.com>
2025-05-07 18:10:25 +0200

    Refactor CORS middleware: simplify origin handling to allow all requests for debugging, and clean up commented-out code for accepted origins.

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,29 +1,41 @@
 import cors from 'cors'
 
-const ACCEPTED_ORIGINS = [
-  'http://localhost:8080',
-  'http://localhost:8081',
-  'http://localhost:19006',
-  'http://127.0.0.1:8081',
-  'http://172.22.32.1:8081'
-]
+// const ACCEPTED_ORIGINS = [
+//   'http://localhost:8080',
+//   'http://localhost:8081',
+//   'http://localhost:19006',
+//   'http://127.0.0.1:8081',
+//   'http://172.22.32.1:8081', // Para el emulador móvil
+//   'https://ujbt1l8-j0rgev-8081.exp.direct' // Tu URL de Expo Tunnel
+// ]
 
-export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
+export const corsMiddleware = () =>
   cors({
-    origin: (origin, callback) => {
-      if (acceptedOrigins.includes(origin)) {
-        return callback(null, true)
-      }
-
-      if (!origin) {
-        return callback(null, true)
-      }
-
-      return callback(new Error('Not allowed by CORS'))
-    },
+    origin: '*', // Permite todas las solicitudes (solo para depuración)
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     preflightContinue: false,
     credentials: true,
     exposedHeaders: ['Content-Type', 'Authorization']
   })
+
+// export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
+//   cors({
+//     origin: (origin, callback) => {
+//       if (acceptedOrigins.includes(origin)) {
+//         return callback(null, true)
+//       }
+
+//       if (!origin) {
+//         return callback(null, true) // Permitir solicitudes sin origen (como desde el mismo servidor)
+//       }
+
+//       console.log(`CORS Error: Origin ${origin} not allowed`) // Añadir log para ver el origen
+//       return callback(new Error('Not allowed by CORS'))
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     preflightContinue: false,
+//     credentials: true,
+//     exposedHeaders: ['Content-Type', 'Authorization']
+//   })
